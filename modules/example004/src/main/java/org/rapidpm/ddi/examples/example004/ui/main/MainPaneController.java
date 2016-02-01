986c843be1f158a755da226aa51ecada35cd97c3
Sven Ruppert <sven.ruppert@gmail.com>
2016-02-01 08:48:44 +0100

    changed project header

--- modules/example004/src/main/java/org/rapidpm/ddi/examples/example004/ui/main/MainPaneController.java
+++ modules/example004/src/main/java/org/rapidpm/ddi/examples/example004/ui/main/MainPaneController.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.rapidpm.ddi.examples.example004.ui.main;
 
 import javafx.event.ActionEvent;
@@ -8,14 +27,11 @@ import org.rapidpm.ddi.examples.example004.ui.main.edit.EditPane;
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 
-/**
- * Created by Sven Ruppert on 01.06.2014.
- */
 public class MainPaneController {
 
   @FXML Button btn;
   @FXML EditPane editPane;
-  private int counter = 0;
+  private int counter;
 
   public MainPaneController() {
     System.out.println("MainPaneController created = " + LocalDateTime.now());
--- modules/example004/src/main/java/org/rapidpm/ddi/examples/example004/ui/main/edit/EditPaneController.java
+++ modules/example004/src/main/java/org/rapidpm/ddi/examples/example004/ui/main/edit/EditPaneController.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.rapidpm.ddi.examples.example004.ui.main.edit;
 
 import javafx.event.ActionEvent;
@@ -10,9 +29,6 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 
-/**
- * Created by Sven Ruppert on 01.06.2014.
- */
 public class EditPaneController {
 
 //@Proxy(metrics = true, virtual = true)
@@ -21,7 +37,7 @@ public class EditPaneController {
 
   @FXML Button btn;
   @FXML Label lb;
-  private int counter = 0;
+  private int counter;
 
   public EditPaneController() {
     System.out.println("EditPaneController created " + LocalDateTime.now());
